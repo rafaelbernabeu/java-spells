0dacb5568feea77209932837ec83e3c365c2f986
Rafael Bernabeu <rbbernabeu@gmail.com>
2022-12-23 10:47:57 -0300

    PatternMatchingSwitch.java

---
+++ spells/src/main/java/spells/jdk19/PatternMatchingSwitch.java
@@ -0,0 +1,17 @@
+package spells.jdk19;
+
+public class PatternMatchingSwitch {
+
+    public static void main(String[] args) {
+        Object obj = "Rafael";
+
+        switch (obj){
+            case null -> System.out.println("null");
+            case String s when s.length() > 1 -> System.out.println(s.trim());
+            case Integer i when i > 10 -> System.out.println(i.intValue());
+            case Double d -> System.out.println(d.doubleValue());
+            default -> throw new IllegalArgumentException();
+        }
+    }
+
+}
